3383659b23d512d408fdff27dffa7172f6015c6e
timemeansalot <timemeansalot@gmail.com>
2023-04-23 21:51:47 +0800

    updata

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
-echo "deploy hexo site"
+echo "============ deploy hexo site ============"
 cd hexo-site 
 pwd
 zsh ./d.sh
-echo "push to github"
+echo "============ push to github ============"
 cd ..
 zsh ./cmt.sh
